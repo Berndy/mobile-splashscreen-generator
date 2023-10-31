99832b451b81cb5c1ba56063e3f5f7c48a89dbb7
Alexander Berndt <al-berndt@gmx.de>
2023-10-31 21:33:50 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { compress } from "./image.util";
+import JSZip from "jszip";
 
-function App() {
+const pickFiles = async (accept, multiple) =>
+  new Promise((resolve) => {
+    const inputId = `input-${Math.random().toString(36).substring(2, 15)}`;
+    const input = document.createElement("input");
+    input.id = inputId;
+    input.type = "file";
+    input.multiple = multiple;
+    input.hidden = true;
+    input.value = "";
+    input.accept = accept;
+
+    document.body.appendChild(input);
+    input.addEventListener("change", async (e) => {
+      resolve(e.target.files);
+      input.remove();
+    });
+    input.click();
+  });
+
+const downloadDataUri = (fileName, dataUri) => {
+  const elementId = `a-${Math.random().toString(36).substring(2, 15)}`;
+  const link = document.createElement("a");
+  link.id = elementId;
+  link.hidden = true;
+  link.href = dataUri;
+  link.download = fileName;
+
+  document.body.appendChild(link);
+  link.click();
+};
+
+const generateSplashScreens = async (file) => {
+  const zip = new JSZip();
+
+  const generateSplashScreen = async (name, width, height) => {
+    const blob = await compress(width, height, width / height)(file);
+    zip.folder(name).file("splash.webp", blob);
+  };
+
+  await generateSplashScreen("drawable-land", 800, 480);
+  await generateSplashScreen("drawable-land-hdpi", 800, 480);
+  await generateSplashScreen("drawable-land-ldpi", 320, 200);
+  await generateSplashScreen("drawable-land-mdpi", 480, 320);
+  await generateSplashScreen("drawable-land-xhdpi", 1280, 720);
+  await generateSplashScreen("drawable-land-xxhdpi", 1600, 960);
+  await generateSplashScreen("drawable-land-xxxhdpi", 1920, 1280);
+  await generateSplashScreen("drawable-port-hdpi", 480, 800);
+  await generateSplashScreen("drawable-port-ldpi", 200, 320);
+  await generateSplashScreen("drawable-port-mdpi", 320, 480);
+  await generateSplashScreen("drawable-port-xhdpi", 720, 1280);
+  await generateSplashScreen("drawable-port-xxhdpi", 960, 1600);
+  await generateSplashScreen("drawable-port-xxxhdpi", 1280, 1920);
+
+  const base64string = await zip.generateAsync({ type: "base64" });
+  downloadDataUri(
+    "splashscreens.zip",
+    `data:application/zip;base64,${base64string}`
+  );
+};
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen w-screen flex items-center justify-center">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-5xl py-4 px-8 rounded-md"
+        onClick={async () => {
+          try {
+            setLoading(true);
+            const files = await pickFiles("image/png", false);
+            const file = files[0];
+            await generateSplashScreens(file);
+          } catch (err) {
+            console.log(err);
+          } finally {
+            setLoading(false);
+          }
+        }}
+      >
+        Create Splashscreens
+      </button>
+      {loading && <p>loading...</p>}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/image.util.js
@@ -0,0 +1,139 @@
+import createPica from 'pica';
+
+import ibr from 'image-blob-reduce';
+// const ibr = require('image-blob-reduce');
+
+const pica = createPica();
+const reduceImageSize = ibr(pica);
+reduceImageSize._create_blob = env => pica.toBlob(env.out_canvas, 'image/webp', 1)
+    .then(blob => {
+        // eslint-disable-next-line no-param-reassign
+        env.out_blob = blob;
+        return env;
+    });
+const reduceImageQuality = ibr(pica);
+reduceImageQuality._create_blob = env => pica.toBlob(env.out_canvas, 'image/webp', 0.99)
+    .then(blob => {
+        // eslint-disable-next-line no-param-reassign
+        env.out_blob = blob;
+        return env;
+    });
+
+const reduceImageToBlobSize = (blob, max) => reduceImageSize.toBlob(blob, { max });
+const reduceImageToBlobQuality = (blob, max) => reduceImageQuality.toBlob(blob, { max });
+
+const URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+export const blobToDataUrl = blob => URL.createObjectURL(blob);
+
+const blobToImg = blob => new Promise(resolve => {
+    const imageUrl = blobToDataUrl(blob);
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.onload = () => {
+        resolve(img);
+    };
+});
+
+const cropImgBlob = async (imgBlob, aspectRatio, fileName) => {
+    const image = await blobToImg(imgBlob);
+    const crop = getCropOptions(aspectRatio)(image);
+    if (!crop) {
+        return imgBlob;
+    }
+
+    const canvas = document.createElement('canvas');
+    const pixelRatio = window.devicePixelRatio;
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = crop.width * pixelRatio;
+    canvas.height = crop.height * pixelRatio;
+
+    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
+    ctx.imageSmoothingQuality = 'high';
+
+    ctx.drawImage(
+        image,
+        crop.x,
+        crop.y,
+        crop.width,
+        crop.height,
+        0,
+        0,
+        crop.width,
+        crop.height,
+    );
+
+    return new Promise(resolve => {
+        canvas.toBlob(
+            blob => {
+                if (!blob) {
+                    // reject(new Error('Canvas is empty'));
+                    console.error('Canvas is empty');
+                    return;
+                }
+                // eslint-disable-next-line no-param-reassign
+                blob.name = fileName;
+                resolve(blob);
+            },
+            'image/webp',
+            1,
+        );
+    });
+};
+
+/**
+ * Calculates the options for the HTML image to achieve the given aspect ratio
+ * @returns options object or undefined if no crop is needed
+ */
+const getCropOptions = aspectRatio => image => {
+    const targetHeight = image.naturalWidth / aspectRatio;
+    const targetWidth = image.naturalHeight * aspectRatio;
+
+    if (image.naturalHeight > targetHeight) {
+        // crop height
+        const yPosition = (image.naturalHeight - targetHeight) / 2;
+        return {
+            x: 0, y: yPosition, width: image.naturalWidth, height: targetHeight,
+        };
+    } if (image.naturalWidth > targetWidth) {
+        // crop width
+        const xPosition = (image.naturalWidth - targetWidth) / 2;
+        return {
+            x: xPosition, y: 0, width: targetWidth, height: image.naturalHeight,
+        };
+    }
+
+    return undefined;
+};
+
+/**
+ * Compresses the image blob to the specified max resolution and quality
+ * @returns image blob
+ */
+export const compress = (maxWidth, maxHeight, aspectRatio) => async blob => {
+    // compressing before cropping vastly improves performance
+    const onceCompressedBlob = await reduceImageToBlobSize(blob, Math.max(maxWidth, maxHeight));
+    const croppedBlob = aspectRatio ? await cropImgBlob(onceCompressedBlob, aspectRatio, blob.name) : onceCompressedBlob;
+    // compressing after cropping vastly decreses blob size
+    const compressedBlob = await reduceImageToBlobQuality(croppedBlob, Math.max(maxWidth, maxHeight));
+
+    return compressedBlob;
+};
+
+/**
+ * Compresses the image blob to the regular media size
+ * @returns image blob
+ */
+export const compressImage = compress(1920, 1920);
+
+/**
+ * Compresses the image blob to the regular media size
+ * @returns image blob
+ */
+export const compressTileBackground = compress(1280, 1280, 16 / 9);
+
+/**
+ * Compresses the image blob to thumbnail size
+ * @returns image blob
+ */
+export const compressThumbnail = compress(128, 128);
