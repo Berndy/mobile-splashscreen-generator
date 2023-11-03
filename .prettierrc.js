b9359ad7f0b88c2a7a3dd25811963e7d7d72aab8
Berndy <al-berndt@gmx.de>
2023-11-03 17:53:43 +0100

    added prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/** @format */
+
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    insertPragma: false,
+    requirePragma: false,
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    quoteProps: 'consistent',
+};
